219a373ef77b78ae274f712b20ea4a3b0e6f807d
CENK <CENK@DESKTOP-TN6457P>
2020-07-28 14:23:29 +0300

    Discard

--- packages/mobile/__tests__/App.spec.js
+++ packages/mobile/__tests__/App.spec.js
@@ -7,7 +7,7 @@ import React from 'react';
 
 import {App} from 'components/src/App';
 
-jest.mock("react-native-video", () => "Video");
+jest.mock('react-native-video', () => 'Video');
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
