5f884d39abf01f74a72e3901a15d921efc578679
EMRE ALICI (TECHNOLOGY) <DTEALICI@digiturk.com.tr>
2020-08-11 08:43:54 +0300

    move keybinder to handler

--- packages/reduxstore/src/screens/Handler.js
+++ packages/reduxstore/src/screens/Handler.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import { KeyBinder, keysReducer } from 'key-event-handler';
 const Handler = ({ network, theme }) => {
   if (network.offline && !!network.isInitiated) alert('network offline');
   if (network.online && !!network.isInitiated) alert('network online');
-  return null;
+  return (
+    <KeyBinder
+      store={keysStore}
+      onVolUp={() => {
+        console.log('on vol up - global');
+      }}
+      onVolDown={() => {
+        console.log('on vol down - global');
+      }}
+    />
+  );
 };
 
 const mapStateToProps = (state) => ({
--- packages/reduxstore/src/setup.js
+++ packages/reduxstore/src/setup.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import createStore from './redux/store/createStore';
-import { KeyBinder, keysReducer } from 'key-event-handler';
 import { LocaleContextProvider } from 'i18n/LocaleContext';
 import Handler from './screens/Handler';
 
@@ -17,15 +16,6 @@ export default function setupApp(App) {
         <PersistGate persistor={store.persistor} loading={null}>
           <LocaleContextProvider>
             <Provider store={keysStore}>
-              <KeyBinder
-                store={keysStore}
-                onVolUp={() => {
-                  console.log('on vol up - global');
-                }}
-                onVolDown={() => {
-                  console.log('on vol down - global');
-                }}
-              />
               <Handler />
               <App />
             </Provider>
